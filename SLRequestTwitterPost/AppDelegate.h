d994a27eb4d9b132ef43638dc97eaf6b1d0be9f0
Pradip <pradip@Pradips-Mac.local>
2015-06-09 22:35:33 +0530

    Initial Commit

---
+++ SLRequestTwitterPost/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLRequestTwitterPost
+//
+//  Created by Pradip on 6/9/15.
+//  Copyright (c) 2015 Pradip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
